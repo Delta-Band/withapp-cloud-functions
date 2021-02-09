d977b0282398c65c458d0da727ff3115d1b2cc67
Rohith Gilla <gillarohith1@gmail.com>
2021-02-09 15:34:49 +0530

    new story notifications. (#3)

---
+++ functions/background/onStoryCreate.js
@@ -0,0 +1,48 @@
+const admin = require("firebase-admin");
+
+function createNotification(name, cover) {
+  return {
+    title: `${name} has started an new story.`,
+    body: "Jump into see what they are up to!",
+    image: cover === undefined ? "" : cover,
+  };
+}
+
+async function onStoryCreateImpl(snapshot, context) {
+  const storyData = snapshot.data();
+
+  const ownerRef = await admin
+    .firestore()
+    .doc(`users/${storyData.owner}`)
+    .get();
+
+  const ownerData = ownerRef.data();
+
+  const notification = createNotification(
+    ownerData.customDisplayName === undefined
+      ? ownerData.display_name
+      : ownerData.customDisplayName,
+    storyData.cover
+  );
+
+  if (ownerData.notifiers.newStories !== undefined) {
+    const promises = [];
+    ownerData.notifiers.newStories.forEach((uid) => {
+      promises.push(
+        admin.messaging().sendToTopic(uid, {
+          data: {
+            storyId: context.params.storyId,
+            click_action: "FLUTTER_NOTIFICATION_CLICK",
+          },
+          notification,
+        })
+      );
+    });
+
+    await Promise.all(promises);
+  }
+}
+
+module.exports = {
+  onStoryCreateImpl,
+};
